626b878eca5a9d60f2831c18bb41910d7a22163a
czz <czzonet@163.com>
2018-06-05 11:34:51 +0800

    abound commit---增加算法逻辑注释

--- BCD.c
+++ BCD.c
@@ -15,18 +15,22 @@
 #include "stdio.h"
 #include "BCD.h"
 
+//直接转换为十进制数
 uint8_t bcd_str_to_int(char bcd[]){
     return atoi(bcd);
 }
 
+//使用网上的精妙算法进行运算逻辑简化
 uint8_t bcd_number_to_int(uint8_t bcd){
     return bcd-(bcd >> 4)*6;
 }
 
+//必须要分开格式化以保证0不被省略
 void int_to_bcd_str(uint8_t num,char result[]){
     sprintf(result,"%d%d",num/10,num%10);
 }
 
+//分取高低四位赋值即可
 uint8_t int_to_bcd_number(uint8_t num){
     return ((num/10) << 4) | ((num%10) & 0x0F);
 }
